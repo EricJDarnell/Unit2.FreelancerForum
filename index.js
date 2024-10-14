c38490c1840e438e921c2d513884095cb1f905e9
EricJDarnell <eric.j.darnell@gmail.com>
2024-10-14 00:59:09 -0400

    changed render method

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-class Freelancer{
-    constructor(name, occupation, startingPrice){
-        this.name = name;
-        this.occupation = occupation;
-        this.startingPrice = startingPrice;
-    }
+class Freelancer {
+  constructor(name, occupation, startingPrice) {
+    this.name = name;
+    this.occupation = occupation;
+    this.startingPrice = startingPrice;
+  }
 }
-const freelancers=[
-    { name: 'Alice', occupation: 'Writer', startingPrice: 30 }
-]
+const freelancers = [
+  { name: "Alice", occupation: "Writer", startingPrice: 30 },
+];
 
-const freelancerPool = []
+const freelancerPool = [];
 freelancerPool.push(new Freelancer(`Bob`, `Teacher`, 50));
 freelancerPool.push(new Freelancer(`Carol`, `Programmer`, 70));
 freelancerPool.push(new Freelancer(`Yoshi`, `Artist`, 60));
-freelancerPool.push(new Freelancer(`Old Scratch`, `Fiddle Lessons`, 666))
+freelancerPool.push(new Freelancer(`Old Scratch`, `Fiddle Lessons`, 666));
 freelancerPool.push(new Freelancer(`Gabor`, `Doctor`, 350));
 freelancerPool.push(new Freelancer(`Tifa`, `Personal Trainer`, 70));
 freelancerPool.push(new Freelancer(`George`, `Author`, 150));
@@ -30,41 +30,36 @@ freelancerPool.push(new Freelancer(`James`, `Amatuer Investigator`, 20));
 freelancerPool.push(new Freelancer(`Nanaki`, `Good boy`, 0));
 freelancerPool.push(new Freelancer(`Ned`, `Headless Winter Enthusiast`, 80));
 
-
 console.log(freelancerPool);
 
 let i = 0;
 
-function updateAverage(){
-let sum = freelancers.reduce((sum, free) => sum + free.startingPrice, 0);
-    let average = sum / freelancers.length;
-    average = Math.floor(average * 100)/100;
-    const averageDisplay = document.querySelector("#averageStartingPrice");
-    averageDisplay.innerText = average;
+function updateAverage() {
+  let sum = freelancers.reduce((sum, free) => sum + free.startingPrice, 0);
+  let average = sum / freelancers.length;
+  average = Math.floor(average * 100) / 100;
+  const averageDisplay = document.querySelector("#averageStartingPrice");
+  averageDisplay.innerText = average;
 }
 
 function render() {
-    if(i == freelancerPool.length){
-        clearInterval(addFreelancerIntervalId);
-    }
-    const table = document.querySelector(".table");
-    const freelancerDiv = document.createElement("div");
-    freelancerDiv.classList.add("freelancer");
-    table.appendChild(freelancerDiv);
-    const nameSpan = document.createElement("span");
-    nameSpan.textContent = `${freelancerPool[i].name}`;
-    const occupationSpan = document.createElement("span")
-    occupationSpan.textContent = `${freelancerPool[i].occupation}`;
-    const priceSpan = document.createElement("span");
-    priceSpan.textContent = `$${freelancerPool[i].startingPrice}`;
-    freelancerDiv.appendChild(nameSpan);
-    freelancerDiv.appendChild(occupationSpan);
-    freelancerDiv.appendChild(priceSpan);
-    
-    freelancers.push(freelancerPool[i]);
-    updateAverage();
+  if (i == freelancerPool.length) {
+    clearInterval(addFreelancerIntervalId);
+  }
+  const table = document.querySelector(".table");
+  const freelancerDiv = document.createElement("div");
+  freelancerDiv.classList.add("freelancer");
+  table.appendChild(freelancerDiv);
+  freelancerDiv.innerHTML = `
+      <span>${freelancerPool[i].name}</span>
+      <span>${freelancerPool[i].occupation}</span>
+      <span>$${freelancerPool[i].startingPrice}</span>
+    `;
+
+  freelancers.push(freelancerPool[i]);
+  updateAverage();
 }
 const addFreelancerIntervalId = setInterval(() => {
-    render();
-    i++;
+  render();
+  i++;
 }, 3000);
